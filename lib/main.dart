0751044abe5913c790bc4c7e924514dca03b5841
bigboss09niel <ronielmadrigal.rm@gmail.com>
2024-07-01 11:33:46 +0800

    Activity 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:piggytechfront/home.dart';
 import 'package:piggytechfront/pages/dashboard.dart';
 import 'package:piggytechfront/pages/menu.dart';
+import 'package:piggytechfront/pages/signup.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  initialRoute: '/menu',
+  initialRoute: '/signup',
   routes: {
     '/' : (context) => Dashboard(),
     '/menu' : (context) => Menu(),
-    '/profile' : (context) =>Home(),
+    '/profile' : (context) => Home(),
+    '/signup' : (context) => Signup(),
   },
 ));
